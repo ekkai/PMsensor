d392887e7e7c2ddbe4037a788dbbf3baf017a63f
Woo jonghyun <wjh3458@nate.com>
2019-03-07 10:45:26 +0900

    예제파일 수정

--- examples/PMsensorDefault/PMsensorDefault.ino
+++ examples/PMsensorDefault/PMsensorDefault.ino
@@ -4,24 +4,23 @@ PMsensor PM;
 
 void setup() {
   Serial.begin(9600);
-  
+
   /////(infrared LED pin, sensor pin)  /////
-  PM.init(3, A0);          
+  PM.init(3, A0);
 }
 
 void loop() {
   Serial.println("=================================");
   Serial.println("Read PM2.5");
+
+  float filter_Data = PM.read(0.1);
+  float noFilter_Data = PM.read();
+
+  Serial.print("Filter : ");
+  Serial.println(filter_Data);
+  Serial.print("noFilter : ");
+  Serial.println(noFilter_Data);
+
   
-  float data = 0;
-  int err = PMsensorErrSuccess;
-  if ((err = PM.read(&data, true, 0.1)) != PMsensorErrSuccess) {
-    Serial.print("data Error = ");
-    Serial.println(err);
-    delay(3000);
-    return;
-  }
-  
-  Serial.println(data);
-  delay(3000);
+  delay(1000);
 }
